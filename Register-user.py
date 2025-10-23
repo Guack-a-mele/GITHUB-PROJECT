fae393f6e99c9bc47041664fd439548e656328e8
Guack-a-Mele <neelesh_081@outlook.com>
2025-10-23 22:44:26 +0530

    name change

---
+++ Register-user.py
@@ -0,0 +1,10 @@
+names=[]
+def Register_user():
+    id=input("Your name ")
+    pw=input("Password ")
+    \
+    for i in range (names):
+        if id==i:
+            print("Name already exists ")
+        else:
+            print("Profile created")
