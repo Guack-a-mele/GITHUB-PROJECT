3ea72d84b666c4f0de4ff83738f2717b47304b26
Guack-a-Mele <neelesh_081@outlook.com>
2025-10-23 22:48:51 +0530

    Removed /

--- Register-user.py
+++ Register-user.py
@@ -2,7 +2,6 @@ names=[]
 def Register_user():
     id=input("Your name ")
     pw=input("Password ")
-    \
     for i in range (names):
         if id==i:
             print("Name already exists ")
