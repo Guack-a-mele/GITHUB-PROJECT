5ae5691bace3fdc67cd1012f701d230705a6dd05
Guack-a-Mele <neelesh_081@outlook.com>
2025-11-17 00:20:30 +0530

    update another-main.py

--- another-main.py
+++ another-main.py
@@ -3,7 +3,21 @@ now = datetime.now()
 items_ordered= []
 menu_items = {}
 total=0
+'''
+qitemd={}
 
+with open("qitemd.txt", "r+") as qitem:
+        while True:
+            try:
+                for i, line in enumerate(qitem, start=1):
+                    if "-" in line:
+                        ab=qitem.readline()
+                        abc=(len(ab)-1)
+                        abcd,cd=line.split("-")
+                        qitemd[cd]=qitemd[cd]-qty
+            except EOFError:
+                break
+'''      
 with open("impmenu.txt", "r") as file:
     for i, line in enumerate(file, start=1): # learn this (NKS)
         if "-" in line:
@@ -12,7 +26,7 @@ with open("impmenu.txt", "r") as file:
 
 print("==========================================[[WELCOME]]===========================================")
 print("1. To view Menu enter \"Menu\" or \"m\"")
-print("2. To add to cart enter the item's Sno")
+print("2. To add to cart enter the item's Serial Number")
 print("3. To view cart enter view cart")
 print("4. Invoice I or i")
 while True: 
@@ -22,7 +36,7 @@ while True:
     if a in ("Menu", "m"):
         with open("Menu.txt","r") as menu:
             print(menu.read())
-    elif a in ("1","5","6","7","8"):
+    elif a in ("1","2","5","6","7","8"):
         item, price= menu_items[a]
         print("Name:", item)
         print("Price:", price)
@@ -34,8 +48,8 @@ while True:
                 try:
                     qty = int(input("Enter quantity --> "))
                     total = total + (price * qty)
-                    items_ordered.append((item, qty, total))
-                    print(name,"added !",price, "x", qty, "=", total)
+                    items_ordered.append((item, qty, price*qty))
+                    print(item,"added !",price, "x", qty, "=", price*qty)
                     break
                 except ValueError:
                      print("That ain't a number !?!?!?")
@@ -43,10 +57,10 @@ while True:
             print("sad --(*)_(*)--")
         else:
             print("Skipping...")
-    elif a in ("2","3","4","5",):
+    elif a in ("3","4","5",):
         item, price= menu_items[a]
-        print("Name:", item)
-        print("Price:", price,"/",price+25)
+        print("Name:", item,"[S/M]")
+        print("Price:", price, "[S]","/",price+25,"[M]")
         print("Availability: Available")
         print("Would you like to add it to cart")
         b=input("--> ")
@@ -54,54 +68,64 @@ while True:
             print("Small or Medium")
             c=input("S/M--> ")
             if c in ("Small", "S","s"):
-                print(name,"[S]","added !",price, "x", qty, "=", total)
                 while True:
                     try:
                         qty = int(input("Enter quantity --> "))
                         total = total + (price * qty)
-                        items_ordered.append((item+" [S]", qty, total))
-                        print(name,"added !",price, "x", qty, "=", total)
+                        items_ordered.append((item+"[S]", qty, price*qty))
+                        print(item,"[S]","added !",price, "x", qty, "=", price*qty)
                         break
                     except ValueError:
                         print("That ain't a number !?!?!?")
             elif c in ("Medium","M","m"):
-                        print(name,"[S]","added !",price, "x", qty, "=", total)
                 while True:
                     try:
                         qty = int(input("Enter quantity --> "))
                         total = total + (price * qty)
-                        items_ordered.append((item+" [S]", qty, total))
-                        print(name,"added !",price, "x", qty, "=", total)
+                        items_ordered.append((item+"[M]", qty, price*qty))
+                        print(item,"[M]","added !",price, "x", qty, "=", price*qty)
                         break
                     except ValueError:
                         print("That ain't a number !?!?!?")  
     elif a in ("Invoice", "I","i"):
-        print("Enter details for Invoice")
-        name=input("Name ")
-        while True:
-            try:
-                phone=int(input("Phone number "))
-                break
-            except ValueError:
-                print("Enter the number damnit")
-        address=input("Address ")
-        with open("filename.txt", "w+") as file:
-            file.write("======= McDonald's Delivery Invoice =======\n")
-            file.write(f"Date: {now.strftime('%d-%m-%Y %H:%M:%S')}\n")
-            file.write(f"Customer Name: {name}\n")
-            file.write(f"Phone: {phone}\n")
-            file.write(f"Address: {address}\n")
-            file.write("-------------------------------------------\n")
-            file.write("Items Ordered:\n")
+        if total==0:
+            print("Your cart is empty, please add something first")
+            print("aaaplaceholder")
+        else:
+            print("Enter details for Invoice")
+            name=input("Name ")
+            while True:
+                try:
+                    phone=int(input("Phone number "))
+                    break
+                except ValueError:
+                    print("Enter the number damnit")
+            address=input("Address ")
+            with open("filename.txt", "w+") as file:
+                file.write("======= McDonald's Delivery Invoice =======\n")
+                file.write(f"Date: {now.strftime('%d-%m-%Y %H:%M:%S')}\n")
+                file.write(f"Customer Name: {name}\n")
+                file.write(f"Phone: {phone}\n")
+                file.write(f"Address: {address}\n")
+                file.write("-------------------------------------------\n")
+                file.write("Items Ordered:\n")
 
+                for item_name, qty, amount in items_ordered:
+                    file.write(f"{item_name} x{qty} = {amount}\n")
+
+                file.write("-------------------------------------------\n")
+                file.write(f"Total Bill: ₹{total}\n")
+                file.write("Thank you for ordering from McDonald's!\n")
+                file.write("Your food will be delivered shortly 🍟🍔\n")
+                file.seek(0)
+                print(file.read())
+    elif a in("cart"):
+        if total==0:
+            print("Nothing in cart")
+        else:
+            count=1
             for item_name, qty, amount in items_ordered:
-                file.write(f"{item_name} x{qty} = {amount}\n")
-
-            file.write("-------------------------------------------\n")
-            file.write(f"Total Bill: ₹{total}\n")
-            file.write("Thank you for ordering from McDonald's!\n")
-            file.write("Your food will be delivered shortly 🍟🍔\n")
-            file.seek(0)
-            print(file.read())
+                print(count,item_name, "qty", qty,'amount', amount)
+                count += 1
     else:
         print("Please choose something from the given options")
